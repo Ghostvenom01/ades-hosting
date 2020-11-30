653aece2cd44f4f9519de219b8c41925bd4827de
Kokkaku21 <izacnino.19@ichat.sp.edu.sg>
2020-11-30 20:07:17 +0800

    App.JS Edit

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(cors());
  * ========================== CODE STARTS HERE =========================
  * =====================================================================
  */
+const { Client } = require('pg');
 
 /**
  * ========================== SETUP APP =========================
@@ -21,6 +22,46 @@ app.use(cors());
 /**
  * JSON Body
  */
+const errors = {
+    QUEUE_EXISTS: {
+        body: { error: 'Queue already exists', code: 'QUEUE_EXISTS' },
+        status: 400,
+    },
+    INVALID_BODY: {
+        body: { error: 'Something wrong with your request body', code: 'INVALID_JSON_BODY' },
+        status: 400,
+    },
+    NON_EXISTENT:{
+        body: {error: 'Queue not found', code: 'UNKNOWN_QUEUE'},
+        status: 404
+    },
+    ALREADY_IN: {
+        body: { error: 'Customer already in Queue', code: 'ALREADY_IN_QUEUE' },
+        status: 422,
+    },
+    WRONG_ID: {
+       body: {
+            error: "Customer Id should be 10-digits",
+            code: "INVALID_JSON_BODY"
+        },
+        status: 400
+    },
+    QUEUE_ALREADY:{
+        body: { error: 'Queue already exists', code: 'QUEUE_EXISTS' },
+        status: 422,
+    },
+    WRONG_ID2: {
+        body: {
+             error: "Company Id should be 10-digits",
+             code: "INVALID_JSON_BODY"
+         },
+         status: 400
+     },
+     SERVER_ERROR: {
+        body: { error: 'Unable to establish connection with database', code: 'UNEXPECTED_ERROR' },
+        status: 500,
+    },
+};
 
 /**
  * ========================== RESET API =========================
@@ -37,7 +78,39 @@ app.use(cors());
 /**
  * Company: Create Queue
  */
-
+app.post('company/queue', function (req,res) {
+    const client = new Client({
+        host: 'john.db.elephantsql.com',
+        port: '5432',
+        user: 'butbeajc',
+        password: 'rMQNj4puKBx13X26hwFCwZ6kelmkAqWX',
+        database: 'butbeajc',
+    });
+    let company_id = req.body.company_id;
+    let queue_id = req.body.queue_id;
+    client.connect();
+    const sql = `Insert into queue(company_id, queue_id) values($1,$2)`;
+    client.query(sql, [company_id, queue_id]).then(function (result) {
+        client.end();
+        if (company_id.length != 10) {
+            res.status(errors.WRONG_ID2.status);
+            res.send(errors.WRONG_ID2);
+        }
+        else{
+            res.status(201)
+            console.log(result);
+        }
+        
+    }).catch(function (error) {
+        if (error == 'ER_DUP_ENTRY') {
+            res.status(errors.QUEUE_ALREADY.status);
+            res.send(errors.QUEUE_ALREADY);
+        }else {
+            res.status(errors.SERVER_ERROR.status);
+            res.send(errors.SERVER_ERROR);
+        }
+    })
+})
 /**
  * Company: Update Queue
  */
@@ -57,10 +130,79 @@ app.use(cors());
 /**
  * Customer: Join Queue
  */
+app.post('customer/queue', function (req,res) {
+    const client = new Client({
+        host: 'john.db.elephantsql.com',
+        port: '5432',
+        user: 'butbeajc',
+        password: 'rMQNj4puKBx13X26hwFCwZ6kelmkAqWX',
+        database: 'butbeajc',
+    });
+    let customer_id = req.body.customer_id;
+    let queue_id = req.body.queue_id;
+    client.connect();
+    const sql = `Insert into queue(customer_id, queue_id) values($1,$2)`;
+    client.query(sql, [customer_id, queue_id]).then(function (result) {
+        client.end();
+        if (customer_id.lengths != 10) {
+            res.status(errors.WRONG_ID.status);
+            res.send(errors.WRONG_ID)
+        }
+        else{
+            res.status(201);
+            res.send(result);
+        }
+    }).catch(function (error) {
+        if (error == 'ER_BAD_FIELD_ERROR') {
+            res.status(errors.ALREADY_IN.status);
+            res.send(errors.ALREADY_IN);
+        } 
+        else{
+            res.status(errors.SERVER_ERROR.status);
+            res.send(errors.SERVER_ERROR);
+        }
+    })
+})
+    
 
 /**
  * Customer: Check Queue
  */
+app.get('customer/queue', function (res, req) { 
+    const client = new Client({
+        host: 'john.db.elephantsql.com',
+        port: '5432',
+        user: 'butbeajc',
+        password: 'rMQNj4puKBx13X26hwFCwZ6kelmkAqWX',
+        database: 'butbeajc',
+    });
+    let customer_id = req.body.customer_id;
+    let queue_id = req.body.queue_id;
+    client.connect();
+    const sql = `SELECT * from queue`; //Do for loop to find position for the ahead
+    client.query(sql, [customer_id, queue_id]).then(function (result) {
+        const customerID = [];
+            for (let i = 0; i < result.rows.length; i++) {
+                customerID.push(result.rows[i].customer_id);
+            }
+        let a = customerID.indexOf(customer_id);
+        let ahead = 0
+        for (let i = 0; i < customerID.length; i++) {
+            ahead = ahead + 1
+            if (customerID[i] == customerID[a]) {
+                break
+            }
+        }
+        ahead = ahead - 1
+        console.log(ahead)
+        
+    }).catch(function (error) {
+        console.log(error)
+        
+    }) 
+    client.end();
+})
+  
 
 /**
  * ========================== UTILS =========================
