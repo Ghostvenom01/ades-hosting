5764a251291b2a5aa92542d9a4dcf7bd793e07ce
Kokkaku21 <izacnino.19@ichat.sp.edu.sg>
2020-12-01 22:14:24 +0800

    lol

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var app = require('./app.js');
 var port = 5432;
 
 var server = app.app.listen(port, function(){
-    console.log('Web App Hosted at http://localhost:%s', port);
+    console.log('Web App Hosted at http://localhost:%s', port)
 });
