1d92000690bdca50d2f2d4042e31457429e3f3be
binhui <60148258+Ghostvenom01@users.noreply.github.com>
2020-12-04 17:14:56 +0800

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = require('./app.js');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5432;
 
 var server = app.app.listen(port, function(){
     console.log('Web App Hosted at http://localhost:%s', port)
